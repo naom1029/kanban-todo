c180b9798e5630d648797762419fe3913be0ba48
naom1029 <bluesky.97102945@gmail.com>
2024-11-04 20:02:27 +0900

    ファイル名変更 app-sidebar.tsx -> AppSidebar.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app-sidebar";
+import { AppSidebar } from "@/components/AppSidebar";
 import "../styles/global.css";
 export default function RootLayout({
   children,
